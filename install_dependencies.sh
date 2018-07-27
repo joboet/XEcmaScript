1cc3947e1a5f27ad887d197f038a62aa339de885
CoffeeBlend <jonasboettiger@icloud.com>
2018-07-27 13:36:53 +0200

    Bug fixing

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,11 +3,13 @@
 if [ $TRAVIS_OS_NAME == osx ]
 then
   brew install ccache boost yasm gawk ccache python autoconf@2.13
+  export AUTOCONF=autoconf213
   export LOADERPATH="@loader_path"
 fi
 if [ $TRAVIS_OS_NAME == linux ]
 then
-  sudo apt-get install pkg-config boost perl yasm binutils-avr autoconf@2.13
+  sudo apt-get install pkg-config libboost-filesystem-dev perl yasm binutils-avr autoconf2.13
+  export AUTOCONF=autoconf2.13
   export LOADERPATH="\$ORIGIN"
 fi
 
@@ -23,10 +25,13 @@ unzip SDK.zip
 rm SDK.zip
 git clone -b esr52 --depth 1 https://github.com/mozilla/gecko-dev.git
 cd gecko-dev/js/src/
+$AUTOCONF
+mkdir buildRLS
+cd buildRLS
 ../configure --prefix=/usr/local --disable-js-shell --disable-tests
 make -j4
 make install
-cd ../../../
+cd ../../../../
 rm -R gecko-dev
 install_name_tool -id $LOADERPATH/lib/libmozjs.dylib /usr/local/lib/libmozjs-52.dylib
 
