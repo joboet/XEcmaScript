53ec15e7f20c8b446f54aa6f26414b1268c2aa66
CoffeeBlend <jonasboettiger@icloud.com>
2018-07-27 16:25:45 +0200

    Bug fixing

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,38 +1,37 @@
 #!/usr/bin/env bash
 
-if [ $TRAVIS_OS_NAME == osx ]
-then
-  brew install ccache boost yasm gawk ccache python autoconf@2.13
-  export AUTOCONF=autoconf213
-  export LOADERPATH="@loader_path"
-fi
 if [ $TRAVIS_OS_NAME == linux ]
 then
-  sudo apt-get install pkg-config libboost-filesystem-dev perl yasm binutils-avr autoconf2.13
-  export AUTOCONF=autoconf2.13
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+  echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >> ~/.profile
   export LOADERPATH="\$ORIGIN"
 fi
+
+if [ $TRAVIS_OS_NAME == osx ]
+then
+  export LOADERPATH="@loader_path"
+fi
+
+brew install pkg-config perl binutils ccache boost yasm gawk python autoconf@2.13
 
 if [ ! -d deps ]
 then
 
 mkdir deps
 cd deps
-mkdir lib
-mkdir include
 curl http://developer.x-plane.com/wp-content/plugins/code-sample-generation/sample_templates/XPSDK301.zip > SDK.zip
-unzip SDK.zip
+unzip -qq SDK.zip
 rm SDK.zip
 git clone -b esr52 --depth 1 https://github.com/mozilla/gecko-dev.git
 cd gecko-dev/js/src/
-$AUTOCONF
+autoconf213
 mkdir buildRLS
 cd buildRLS
-../configure --prefix=/usr/local --disable-js-shell --disable-tests
+../configure --prefix=/usr/local --disable-tests
 make -j4
 make install
 cd ../../../../
-rm -R gecko-dev
+sudo rm -R gecko-dev
 install_name_tool -id $LOADERPATH/lib/libmozjs.dylib /usr/local/lib/libmozjs-52.dylib
 
 fi
