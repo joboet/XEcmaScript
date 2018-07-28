d6e450824f9de48a7b64fdddb28dfac3aa7b960a
CoffeeBlend <jonasboettiger@icloud.com>
2018-07-28 10:11:15 +0200

    Let's try this

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,15 +5,7 @@ then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
   sudo apt-get install build-essential
   export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-  export LOADERPATH="\$ORIGIN"
-  brew install patchelf
-  export CHANGEID=patchelf --set-soname \$ORIGIN/lib/libmozjs.so /usr/local/lib/libmozjs-52.so
-fi
-
-if [ $TRAVIS_OS_NAME == osx ]
-then
-  export LOADERPATH="@loader_path"
-  export CHANGEID=install_name_tool -id @loader_path/lib/libmozjs.dylib /usr/local/lib/libmozjs-52.dylib
+  brew install patchelf gcc
 fi
 
 brew install pkg-config perl binutils ccache boost yasm gawk python autoconf@2.13
@@ -31,11 +23,23 @@ cd gecko-dev/js/src/
 autoconf213
 mkdir buildRLS
 cd buildRLS
+if [ $TRAVIS_OS_NAME == linux ]
+then
+  export CC=gcc-8
+  export CXX=g++-8
+fi
 ../configure --prefix=/usr/local --disable-tests
 make -j4 -s
 make install
 cd ../../../../
 sudo rm -R gecko-dev
-bash -c $CHANGEID
 
+if [ $TRAVIS_OS_NAME == osx ]
+then
+  install_name_tool -id @loader_path/lib/libmozjs.dylib /usr/local/lib/libmozjs-52.dylib
+fi
+if [ $TRAVIS_OS_NAME == linux ]
+then
+  pathelf --set-soname \$ORIGIN/lib/libmozjs.so /usr/local/lib/libmozjs-52.so
+fi
 fi
