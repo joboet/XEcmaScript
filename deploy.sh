4cacf5776feec53866d971275735ccc6a5b19111
CoffeeBlend <jonasboettiger@icloud.com>
2018-08-19 23:33:23 +0200

    Added automatic deployment

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+export $BUILDDIR=$(pwd)/build
+
+if [ $TRAVIS_OS_NAME == mac ]
+then
+  export PLUGIN_NAME=mac.xpl
+  export EXTENSION=.dylib
+fi
+if [ $TRAVIS_OS_NAME == linux ]
+then
+  export PLUGIN_NAME=lin.xpl
+  export EXTENSION=.so
+fi
+
+export MOZJS_LIB_NAME=$(pkg-config --libs-only-l mozjs-52)
+export MOZJS_LIB_PATH=$(pkg-config --libs-only-L mozjs-52)
+export MOZJS_LIBRARY=${MOZJS_LIB_PATH#"-L"}/lib${MOZJS_LIB_NAME#"-l"}$EXTENSION
+echo $MOZJS_LIBRARY
+
+mkdir deploy && cd deploy
+mkdir $TRAVIS_OS_NAME && cd $TRAVIS_OS_NAME
+mkdir lib
+cp $MOZJS_LIBRARY lib/libmozjs$EXTENSION
+cp $BUILDDIR/$PLUGIN_NAME ./$PLUGIN_NAME
+
+cd ..
+zip -r $TRAVIS_OS_NAME.zip $TRAVIS_OS_NAME
